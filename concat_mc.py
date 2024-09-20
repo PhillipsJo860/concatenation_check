1d80e3bb71670fd173b2c38e8f8096cbb07e34b8
SoggyCereal <PhillipsJo860@northwested.org>
2024-09-20 10:38:48 -0400

    added content

---
+++ concat_mc.py
@@ -0,0 +1,20 @@
+# Joshua Phillips
+# 9/20/24
+# Concatination check
+
+# Task One
+first_name = 'Joshua'
+favorite_game = 'High on Life'
+print('Hello my name is '+first_name+' and I love playing the game '+favorite_game+'.')
+
+
+# Task Two
+print('What I\'ve learned about concatination:')
+print('First is that, it takes up a ton of processing for larger concatinated strings which can lead to lag and even a delay on loading if you use concatination too much. Which is why you are going to mostly use F-Strings.')
+print('Secondly, Concatination looks bad in comparison to an F-String. I used concatination last year in our mini lessons on JavaScript but never got to use F-Strings. I like F-Strings better.')
+
+
+# Task Three
+chicken_fries5pc = 4.79
+
+print('The price of the hip BK item "Chicken fries" is $'+str(chicken_fries5pc)+'.')
